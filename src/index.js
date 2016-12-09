ef5436f516cf50b07eb190219367ee523c402da9
Devon Daviau <devondaviau@yahoo.ca>
2016-12-09 00:24:32 -0500

    Test building JS using Browserify and Uglify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function () {
-    return 'Hello'
-};
+module.exports = chatbot ();
+
+    var botMessage, humanMessage;
+
+    do {
+        botMessage = chatbot(humanMessage);
+        humanMessage = prompt(botMessage);
+    } while (humanMessage.toLowerCase() != 'bye');
+
+    function chatbot (message) {
+        if (message === undefined) {
+            return 'Hello.'
+        }
+    }
+
